5208e353786f3875c368f25608023fb17cbbcb3c
Omutnykh Oleg <omutnih1234@gmail.com>
2021-05-05 22:00:00 +0500

    Изменена кодировка всех файлов лабораторной работы

--- Lab2/employee.py
+++ Lab2/employee.py
@@ -1,4 +1,4 @@
-import person
+﻿import person
 class Employee(person.Person):
 	def __init__(self, surname = "NOVALUE", name = "NOVALUE", patronymic = "NOVALUE", age = 0, salary = 0):
 		super().__init__(surname, name, patronymic, age)
