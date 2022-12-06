7d02c181fac69e24d0313f40661d06439bd35cdc
Omutnykh Oleg <omutnih1234@gmail.com>
2022-12-07 01:59:35 +0500

    Реализована функция my_reduce как переопределённый встроенный reduce

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,8 +1,14 @@
 import csv
 import random
 
-def my_map(function, source_list)
+def my_map(function, source_list):
 	new_list = []
 	for i in source_list:
 		new_list.append(function(i))
 	return new_list
+
+def my_reduce(function, source_list):
+	result = function(source_list[0], source_list[1])
+	for i in source_list[2:]:
+		result = function(result, i)
+	return result
