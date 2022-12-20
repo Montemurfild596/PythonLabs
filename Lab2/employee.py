f50e6274676e5bf8326371250e7854ee36315c92
Omutnykh Oleg <omutnih1234@gmail.com>
2022-12-21 02:07:57 +0500

    Реализованы функции вывода методов класса Employee

--- Lab2/employee.py
+++ Lab2/employee.py
@@ -3,3 +3,9 @@ class Employee(person.Person):
 	def __init__(self, surname = "NOVALUE", name = "NOVALUE", patronymic = "NOVALUE", age = 0, salary = 0):
         super().__init__(surname, name, patronymic, age);
         self.salary = salary;
+
+    def  __str__(self) -> str:
+        return super().__str__() + f"\tДоход: {self.salary}";
+
+    def __repr__(self)-> str:
+        return "Employee: " + self.__str__();
