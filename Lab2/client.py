7ec5817b55f2a52ba82668117bd3584b8a26193c
Omutnykh Oleg <omutnih1234@gmail.com>
2022-12-21 02:16:39 +0500

    Initial commit

--- Lab2/client.py
+++ Lab2/client.py
@@ -1,11 +1,11 @@
 import person
 class Client(person.Person):
     def __init__(self, surname = "NOVALUE", name = "NOVALUE", patronymic = "NOVALUE", age = 0, order= "NOVALUE"):
-        super().__init__(surname, name, patronymic, age);
-        self.order = order;
+        super().__init__(surname, name, patronymic, age)
+        self.order = order
 
     def __str__(self):
-        return super().__str__() + f"\tЗаказ: {self.order}";
+        return super().__str__() + f"\tЗаказ: {self.order}"
 
     def __repr__(self):
-        return "Client: " + self.__str__();
+        return "Client: " + self.__str__()
--- Lab2/company.py
+++ Lab2/company.py
@@ -6,18 +6,18 @@ import copy
 #Компания
 class Company:
     def __init__(self, nam = "NOVALUE", employees = [], clients = []):
-        self.name = name;        
-        self.employees = employees;
-        self.clients = clients;       
+        self.name = name    
+        self.employees = employees
+        self.clients = clients       
 
     def __str__(self):
-        result = f"Компания: {self.name}\nРаботники:\n";
+        result = f"Компания: {self.name}\nРаботники:\n"
         for employee in self.employees:
-            result += employee.__str__() + "\n";
-        result += "Клиенты:\n";
+            result += employee.__str__() + "\n"
+        result += "Клиенты:\n"
         for client in self.clients:
-            result += client.__str__() + "\n";  
-        return result;
+            result += client.__str__() + "\n" 
+        return result
 
     def __repr__(self):
-        return self.__str__();
+        return self.__str__()
