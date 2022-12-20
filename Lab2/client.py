6a0dfca603a67f1c41e5eba86032f8637701d362
Omutnykh Oleg <omutnih1234@gmail.com>
2022-12-21 02:11:10 +0500

    Initial commit

--- Lab2/client.py
+++ Lab2/client.py
@@ -1,5 +1,5 @@
 import person
 class Client(person.Person):
-    def __init__(self, surname : str = "NOVALUE", name : str = "NOVALUE", patronymic : str = "NOVALUE", age : int = 0, order : str = "NOVALUE"):
+    def __init__(self, surname = "NOVALUE", name = "NOVALUE", patronymic = "NOVALUE", age = 0, order= "NOVALUE"):
         super().__init__(surname, name, patronymic, age);
         self.order = order;
