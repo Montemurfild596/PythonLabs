5c5535c7d4f92f4c0b1609dbb8c49008bc3d31b1
Omutnykh Oleg <omutnih1234@gmail.com>
2022-12-21 09:31:20 +0500

    Заполнен файл test.json

--- Lab2/employee.py
+++ Lab2/employee.py
@@ -1,11 +1,11 @@
 import person
 class Employee(person.Person):
 	def __init__(self, surname = "NOVALUE", name = "NOVALUE", patronymic = "NOVALUE", age = 0, salary = 0):
-        super().__init__(surname, name, patronymic, age)
-        self.salary = salary
+		super().__init__(surname, name, patronymic, age)
+		self.salary = salary
 
-    def  __str__(self):
-        return super().__str__() + f"\tДоход: {self.salary}"
+	def  __str__(self):
+		return super().__str__() + f"\tДоход: {self.salary}"
 
-    def __repr__(self):
-        return "Employee: " + self.__str__()
+	def __repr__(self):
+		return "Employee: " + self.__str__()
